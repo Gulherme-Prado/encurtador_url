c01cfc1f31181d2f720042e40987fcf174e10773
Prado <prado.guilherme13@gmail.com>
2025-06-22 19:32:35 -0300

    feat: Iniciar e estruturar pastas do projeto e implementar login com JWT

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule, 
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+
+    async register(dto: CreateUserDto) {
+          const existingUser = await this.usersService.findByEmail(dto.email);
+            if (existingUser) {
+            throw new BadRequestException('Email já cadastrado');
+  }
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+        const user = await this.usersService.create({
+            email: dto.email,
+            password: hashedPassword,
+        });
+        return { message: 'Usuário criado com sucesso', userId: user.id };
+    }
+
+    async login(dto: LoginDto) {
+        const user = await this.usersService.findByEmail(dto.email);
+        if(!user) throw new UnauthorizedException('Credenciais inválidas')
+        
+        const isValid = await bcrypt.compare(dto.password, user.password);
+        if(!isValid) throw new UnauthorizedException('Credenciais inválidas');
+
+        const payload = { sub: user.id, email: user.email };
+        const token = this.jwtService.sign(payload);
+
+        return {access_token: token};
+    }
+}
